75fee13d22de0f992a9c14ef19534dd8ab213b5d
Gabriela Branco <lindosogb@gmail.com>
2024-03-14 20:48:00 +0000

    Decorates strings, custom response if parameters are not specified

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -2,23 +2,35 @@
 
 namespace BetterDecorator
 {
+    /// <summary>
+    /// Adorns the string based on a character chosen by the user, 
+    /// this character is displayed the number of times the user entered.
+    /// </summary>
     class Program
     {
         /// <summary>
-        /// Main method, where the code is executed
+        /// Main method, where the code is executed.
+        /// If the arguments are not specified 
+        /// the method Decor is called with premade parameters
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string decoString = args[0];
-            char decoChar = char.Parse(args[1]);
-            int decoint = int.Parse(args[2]);
-
-            Console.WriteLine( Decor(decoString, decoChar, decoint));
+            if(args.Length < 3)
+            {
+                Console.WriteLine(Decor("User did not specify args!", '=', 3));
+            }
+            else
+            {
+                string decoString = args[0];
+                char decoChar = char.Parse(args[1]);
+                int decoint = int.Parse(args[2]);
+                Console.WriteLine( Decor(decoString, decoChar, decoint));
+            }
         }
         /// <summary>
-        /// Adorns the string based on a character chosen by the user, 
-        /// this character is displayed the number of times the user entered.
+        /// The code takes the string s and decorates with the char dec, that is
+        /// shown a number of times defined by the int o
         /// </summary>
         /// <param name="s"></param>
         /// <param name="dec"></param>
@@ -26,7 +38,7 @@ namespace BetterDecorator
         /// <returns></returns>
         private static string Decor ( string s , char dec, int o)
         {
-            string deco = new string(dec, o); // (char, number of times it repeats)
+            string deco = new string(dec, o);//(char, number of times it repeats)
             return $"{deco} {s} {deco}";
         }
     }
