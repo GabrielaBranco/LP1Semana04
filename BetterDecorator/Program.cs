8abbd2440edc93b223eb329a6a972171f43a0db3
Gabriela Branco <lindosogb@gmail.com>
2024-03-14 20:14:46 +0000

    Decorates texts

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decoString = args[0];
+            char decoChar = char.Parse(args[1]);
+            int decoint = int.Parse(args[2]);
+
+            Console.WriteLine( Decor(decoString, decoChar, decoint));
+        }
+        private static string Decor ( string s , char dec, int o)
+        {
+            string deco = new string(dec, o);
+            return $"{deco} {s} {deco}";
+        }
+    }
+}
