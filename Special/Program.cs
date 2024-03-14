4a59b13f388fc587525693d066d42454cf451d91
Gabriela Branco <lindosogb@gmail.com>
2024-03-14 21:12:24 +0000

    Special Sequence

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,31 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert n-th number of the special sequence:");
+            string numberInput = Console.ReadLine();
+            int n = int.Parse(numberInput);
+
+            Console.WriteLine(GetSpecial(n));
+        }
+
+        private static int GetSpecial(int n)
+        {
+            int result;
+
+            if (n == 0)
+            {
+                result = 0;
+            }
+            else if (n == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = GetSpecial(n - 1) +  2 * GetSpecial(n - 2);
+            }
+
+            return result;
         }
     }
 }
