aa19a7bcb3e9a22bbe725307b7ef39098c9d4821
Gabriela Branco <lindosogb@gmail.com>
2024-03-14 20:30:14 +0000

    Decorates texts, now with XML comments

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -4,6 +4,10 @@ namespace BetterDecorator
 {
     class Program
     {
+        /// <summary>
+        /// Main method, where the code is executed
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string decoString = args[0];
@@ -12,6 +16,14 @@ namespace BetterDecorator
 
             Console.WriteLine( Decor(decoString, decoChar, decoint));
         }
+        /// <summary>
+        /// Adorns the string based on a character chosen by the user, 
+        /// this character is displayed the number of times the user entered.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="o"></param>
+        /// <returns></returns>
         private static string Decor ( string s , char dec, int o)
         {
             string deco = new string(dec, o); // (char, number of times it repeats)
